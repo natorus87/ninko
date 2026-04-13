cf7edc544a70d53bd8e81119588a20ecbcc8b750
natorus87 <natorus87@users.noreply.github.com>
2026-04-13 13:45:21 +0200

    fix(dataviz): use modules imports in init

--- backend/modules/dataviz/__init__.py
+++ backend/modules/dataviz/__init__.py
@@ -1,5 +1,5 @@
-from backend.modules.dataviz.manifest import module_manifest
-from backend.modules.dataviz.agent import agent
-from backend.modules.dataviz.routes import router
+from modules.dataviz.manifest import module_manifest
+from modules.dataviz.agent import agent
+from modules.dataviz.routes import router
 
 __all__ = ["module_manifest", "agent", "router"]
